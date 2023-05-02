5577ae2515e06aef52eb5d7162ca3174de4c549d
rubiezzy <rubiezzy@student.21-school.ru>
2023-05-02 12:18:50 +0400

    Add all handlers for user and cinema domains

--- internal/cinema/handler/http.go
+++ internal/cinema/handler/http.go
@@ -15,9 +15,97 @@ func New() httpHandler {
 
 // setRoutes adds handlers to http.DefaultServeMux
 func (h httpHandler) setRoutes() {
-	handler := func(w http.ResponseWriter, _ *http.Request) {
-		w.WriteHeader(http.StatusOK)
+	http.HandleFunc("/movies", moviesHandler)
+	http.HandleFunc("/movies/watched", watchedMoviesHandler)
+	http.HandleFunc("/halls", hallsHandler)
+	http.HandleFunc("/cinema-sessions", sessionsHandler)
+	http.HandleFunc("/tickets", ticketsHandler)
+}
+
+func moviesHandler(w http.ResponseWriter, r *http.Request) {
+	userId := r.URL.Query()
+	if _, ok := userId["movieId"]; ok {
+		if r.Method == "GET" {
+			// return movie by id
+		} else if r.Method == "PUT" {
+			// update movie information (only for admins)
+		} else if r.Method == "DELETE" {
+			// delete movie (only for admins)
+		}
+	} else {
+		if r.Method == "POST" {
+			// create new movie (only for admins)
+		} else if r.Method == "GET" {
+			// return all movies
+		}
 	}
-	http.HandleFunc("/movies", handler)
-	http.HandleFunc("/halls", handler)
+	w.WriteHeader(http.StatusOK)
+}
+
+func watchedMoviesHandler(w http.ResponseWriter, r *http.Request) {
+	userId := r.URL.Query()
+	if _, ok := userId["userId"]; !ok {
+		http.Error(w, "user id not provided: ", http.StatusBadRequest)
+		return
+	}
+
+	// return list of watched movies by user id
+}
+
+func hallsHandler(w http.ResponseWriter, r *http.Request) {
+	hallId := r.URL.Query()
+	if _, ok := hallId["hallId"]; ok {
+		if r.Method == "GET" {
+			// return hall by id
+		} else if r.Method == "PUT" {
+			// update hall information (only for admins)
+		} else if r.Method == "DELETE" {
+			// delete hall (only for admins)
+		}
+	} else {
+		if r.Method == "POST" {
+			// create new hall (only for admins)
+		} else if r.Method == "GET" {
+			// return all halls
+		}
+	}
+	w.WriteHeader(http.StatusOK)
+}
+
+func sessionsHandler(w http.ResponseWriter, r *http.Request) {
+	sessionId := r.URL.Query()
+	if _, ok := sessionId["sessionId"]; ok {
+		if r.Method == "GET" {
+			// return session by id
+		} else if r.Method == "PUT" {
+			// update session information (only for admins)
+		} else if r.Method == "DELETE" {
+			// delete session (only for admins)
+		}
+	} else {
+		if r.Method == "POST" {
+			// create new session (only for admins)
+		} else if r.Method == "GET" {
+			// return all sessions
+		}
+	}
+	w.WriteHeader(http.StatusOK)
+}
+
+func ticketsHandler(w http.ResponseWriter, r *http.Request) {
+	ticketId := r.URL.Query()
+	if _, ok := ticketId["ticketId"]; ok {
+		if r.Method == "GET" {
+			// return ticket by id
+		}
+	} else if _, ok := ticketId["userId"]; ok {
+		if r.Method == "GET" {
+			// return all tickets purchased by the user
+		}
+	} else {
+		if r.Method == "POST" {
+			// create("buy") a new ticket
+		}
+	}
+	w.WriteHeader(http.StatusOK)
 }
--- internal/user/handler/http.go
+++ internal/user/handler/http.go
@@ -15,8 +15,30 @@ func New() httpHandler {
 
 // setRoutes adds handlers to http.DefaultServeMux
 func (h httpHandler) setRoutes() {
-	handler := func(w http.ResponseWriter, _ *http.Request) {
-		w.WriteHeader(http.StatusOK)
+	http.HandleFunc("/auth/login", logIn)
+	http.HandleFunc("/users", userHandler)
+}
+
+func logIn(w http.ResponseWriter, _ *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
+func userHandler(w http.ResponseWriter, r *http.Request) {
+	userId := r.URL.Query()
+	if _, ok := userId["userId"]; ok {
+		if r.Method == "GET" {
+			// return user by id (only for admins)
+		} else if r.Method == "PUT" {
+			// update user information
+		} else if r.Method == "DELETE" {
+			// delete user (only for admins)
+		}
+	} else {
+		if r.Method == "POST" {
+			// create new user
+		} else if r.Method == "GET" {
+			// return all users (only for admins)
+		}
 	}
-	http.HandleFunc("/auth", handler)
+	w.WriteHeader(http.StatusOK)
 }
