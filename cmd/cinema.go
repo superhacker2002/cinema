be2f6f9aa22ca11f68cf989db0389a009b0ff4b8
rubiezzy <rubiezzy@student.21-school.ru>
2023-05-03 14:04:16 +0400

    Add db opening

--- cmd/cinema.go
+++ cmd/cinema.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	auth2 "bitbucket.org/Ernst_Dzeravianka/cinemago-app/internal/auth"
 	cinemaHandler "bitbucket.org/Ernst_Dzeravianka/cinemago-app/internal/cinema/handler"
 	"bitbucket.org/Ernst_Dzeravianka/cinemago-app/internal/config"
 	userHandler "bitbucket.org/Ernst_Dzeravianka/cinemago-app/internal/user/handler"
+	"database/sql"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
@@ -15,8 +17,15 @@ func main() {
 		log.Fatal(err)
 	}
 
+	db, err := sql.Open("postgres", config.Db)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
 	router := mux.NewRouter()
-	userHandler.New(router, config.JWTSecret)
+	auth := auth2.New(db, config.JWTSecret)
+	userHandler.New(router, auth, db)
 	cinemaHandler.New(router)
 
 	log.Fatal(http.ListenAndServe(":"+config.Port, router))
