af7bb826714b3929f784d20b1972537476af84bf
rubiezzy <rubiezzy@student.21-school.ru>
2023-05-03 14:06:04 +0400

    Add auth service

--- go.mod
+++ go.mod
@@ -11,6 +11,8 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/crypto v0.8.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,22 +1,68 @@
 package auth
 
 import (
-	"github.com/dgrijalva/jwt-go"
+	"database/sql"
+	"errors"
 	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	_ "github.com/lib/pq"
 )
 
-type Token struct {
-	Token string `json:"token"`
+type Credentials struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
 }
 
-func GenerateJWT(JWTSecret []byte) (Token, error) {
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"exp": time.Now().Add(time.Hour * 24).Unix(),
-	})
-	tokenString, err := token.SignedString(JWTSecret)
+type User struct {
+	ID       int64  `json:"id"`
+	Username string `json:"username"`
+}
+
+type Auth struct {
+	db        *sql.DB
+	jwtSecret []byte
+}
+
+func New(db *sql.DB, jwtSecret string) Auth {
+	return Auth{
+		db:        db,
+		jwtSecret: []byte(jwtSecret),
+	}
+}
+
+func (a Auth) Authenticate(username string, password string) (string, error) {
+	var passwordHash string
+	var userID int64
+
+	// TODO: get user id from db
+
+	err := comparePasswords(passwordHash, []byte(password))
 	if err != nil {
-		return Token{}, err
+		return "", errors.New("invalid password")
 	}
 
-	return Token{Token: tokenString}, nil
+	return a.generateJWT(userID)
+}
+
+func (a Auth) generateJWT(userID int64) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": userID,
+		"exp":     time.Now().Add(time.Hour * 24).Unix(),
+	})
+	tokenString, err := token.SignedString(a.jwtSecret)
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
+
+func (a Auth) VerifyToken(tokenString string) (*User, error) {
+	// TODO: verify and decode token
+	return &User{}, nil
+}
+
+func comparePasswords(hash string, password []byte) error {
+	// TODO: compare password with its hash
+	return nil
 }
