35036eda6f547527f6c55ab88361075325040794
Ilia <iliamamageishvili@gmail.com>
2023-05-05 11:21:33 +0400

    update password hashes

--- database/mock_data.sql
+++ database/mock_data.sql
@@ -1,8 +1,8 @@
 INSERT INTO users (username, hashed_password, email, credit_card_info, role_id)
-VALUES ('user1', 'vpj3ceeiiFcjz', 'user1@example.com', '1111222233334444', 2);
+VALUES ('user1', 'A3163B169544206384021627139043454DD8C7D926746F6D01A11FA904D90C03', 'user1@example.com', '1111222233334444', 2);
 
 INSERT INTO users (username, hashed_password, email, credit_card_info, role_id)
-VALUES ('user2', 'gcf3ceeiiFyhz', 'user2@example.com', '2222333344445555', 2);
+VALUES ('user2', '5ABFAC4EC9F3459E7FA7C22476615FBA9F2E98125C3D38FDA867993D30735CD8', 'user2@example.com', '2222333344445555', 2);
 
 INSERT INTO movies (title, genre, release_date, duration)
 VALUES ('Avengers: Endgame', 'Action, Adventure, Drama', '2019-04-26', 181);
