a9637a32dce9e707b1fb4c8bc12658920eb25f1f
rubiezzy <rubiezzy@student.21-school.ru>
2023-05-10 14:34:49 +0400

    Add test case fir login service

--- internal/user/handler/http_test.go
+++ internal/user/handler/http_test.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"errors"
 	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -85,6 +86,7 @@ func TestLoginHandler(t *testing.T) {
 		handler(response, req)
 
 		assert.Equal(t, "failed to read request body\n", response.Body.String())
+		assert.Equal(t, http.StatusBadRequest, response.Code)
 	})
 
 	t.Run("unmarshal request fail", func(t *testing.T) {
@@ -97,56 +99,20 @@ func TestLoginHandler(t *testing.T) {
 		handler(response, req)
 
 		assert.Equal(t, "missing username or password\n", response.Body.String())
+		assert.Equal(t, http.StatusBadRequest, response.Code)
+	})
+
+	t.Run("authentication fail", func(t *testing.T) {
+		auth.err = errors.New("something went wrong")
+		req, err := http.NewRequest(http.MethodPost, "auth/login/",
+			strings.NewReader(`{"username": "test_user", "password": "test_password"}`))
+		require.NoError(t, err, "failed to create test request")
+
+		response := httptest.NewRecorder()
+		handler := httpHandler{auth: auth}.loginHandler
+		handler(response, req)
+
+		assert.Equal(t, "failed to authenticate: something went wrong\n", response.Body.String())
+		assert.Equal(t, http.StatusUnauthorized, response.Code)
 	})
 }
-
-//func TestLoginHandler(t *testing.T) {
-//	tests := []struct {
-//		name        string
-//		body        string
-//		auth        *mockAuth
-//		expectedErr string
-//		statusCode  int
-//	}{
-//		//{
-//		//	name: "failed to unmarshal request",
-//		//	body: `{"username": "test_user"}`,
-//		//	auth: &mockAuth{
-//		//		token: "",
-//		//		err:   ErrReadRequestFail,
-//		//	},
-//		//	statusCode: http.StatusBadRequest,
-//		//},
-//		//{
-//		//	name: "failed to authenticate",
-//		//	body: `{"username": "test_user", "password": "test_password"}`,
-//		//	auth: &mockAuth{
-//		//		token: "test_token",
-//		//		err:   errors.New("failed to authenticate"),
-//		//	},
-//		//	statusCode: http.StatusUnauthorized,
-//		//},
-//	}
-//
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			req, err := http.NewRequest(http.MethodPost, "auth/login/", strings.NewReader(tt.body))
-//			require.NoError(t, err, "failed to create test request")
-//
-//			response := httptest.NewRecorder()
-//			handler := httpHandler{auth: tt.auth}.loginHandler
-//
-//			handler(response, req)
-//
-//			body, err := io.ReadAll(response.Body)
-//			assert.NoError(t, err)
-//
-//			var loginResp loginResponse
-//			err = json.Unmarshal(body, &loginResp)
-//			assert.NoError(t, err)
-//
-//			assert.Equal(t, tt.statusCode, response.Code)
-//			assert.Equal(t, tt.auth.token, loginResp.Token)
-//		})
-//	}
-//}
