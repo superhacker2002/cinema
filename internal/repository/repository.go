a5447e33ed40403ae5c110b485d96ca69a7bfaf6
rubiezzy <rubiezzy@student.21-school.ru>
2023-05-11 12:39:41 +0400

    Add role insertion for every created user

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -2,9 +2,7 @@ package repository
 
 import (
 	"bitbucket.org/Ernst_Dzeravianka/cinemago-app/internal/auth"
-	"crypto/sha256"
 	"database/sql"
-	"encoding/hex"
 	"errors"
 	"fmt"
 )
@@ -28,12 +26,13 @@ func (r Repository) User(username string) (auth.Credentials, error) {
 		}
 		return auth.Credentials{}, fmt.Errorf("failed to get user credentials from database: %w", err)
 	}
+
 	return credentials, nil
 }
 
-func (r Repository) CreateUser(username string, password string) (string, error) {
+func (r Repository) CreateUser(username string, passwordHash string, role string) (string, error) {
 	var id string
-	err := r.db.QueryRow("SELECT id FROM users WHERE username = $1", username).Scan(&id)
+	err := r.db.QueryRow("SELECT user_id FROM users WHERE username = $1", username).Scan(&id)
 	if err == nil {
 		return "", fmt.Errorf("user with username %q already exists", username)
 	}
@@ -41,11 +40,14 @@ func (r Repository) CreateUser(username string, password string) (string, error)
 		return "", fmt.Errorf("failed to check if user with username %q exists: %w", username, err)
 	}
 
-	hash := sha256.Sum256([]byte(password))
-	hashHex := hex.EncodeToString(hash[:])
+	var roleID string
+	err = r.db.QueryRow("SELECT role_id FROM roles WHERE role_name = $1", role).Scan(&roleID)
+	if err != nil {
+		return "", fmt.Errorf("failed to get role id from database: %w", err)
+	}
 
-	err = r.db.QueryRow("INSERT INTO users (username, hashed_password) VALUES ($1, $2) RETURNING id",
-		username, hashHex).Scan(&id)
+	err = r.db.QueryRow("INSERT INTO users (username, hashed_password, role_id) VALUES ($1, $2, $3) "+
+		"RETURNING user_id", username, passwordHash, roleID).Scan(&id)
 	if err != nil {
 		return "", fmt.Errorf("failed to insert new user into database: %w", err)
 	}
