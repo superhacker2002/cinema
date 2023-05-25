1024ee29e3064f474232e15150df30255b25a069
rubiezzy <rubiezzy@student.21-school.ru>
2023-05-25 23:27:47 +0400

    Add tests for business logic

--- internal/cinemasessions/service/service_test.go
+++ internal/cinemasessions/service/service_test.go
@@ -1,95 +1,95 @@
 package service
 
 import (
-	"reflect"
+	"bitbucket.org/Ernst_Dzeravianka/cinemago-app/internal/cinemasessions/entity"
+	"errors"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-func TestNew(t *testing.T) {
-	type args struct {
-		r repository
-	}
-	tests := []struct {
-		name string
-		args args
-		want service
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := New(tt.args.r); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("New() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+type mockRepo struct {
+	sessions []entity.CinemaSession
+	err      error
 }
 
-func Test_service_AllSessions(t *testing.T) {
-	type fields struct {
-		r repository
-	}
-	type args struct {
-		date   string
-		offset int
-		limit  int
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		want    []entity.CinemaSession
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			s := service{
-				r: tt.fields.r,
-			}
-			got, err := s.AllSessions(tt.args.date, tt.args.offset, tt.args.limit)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("AllSessions() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("AllSessions() got = %v, want %v", got, tt.want)
-			}
-		})
-	}
+const layout = "2006-01-02 15:04:05 MST"
+
+func (m *mockRepo) AllSessions(date string, offset, limit int) ([]entity.CinemaSession, error) {
+	return m.sessions, m.err
 }
 
-func Test_service_SessionsForHall(t *testing.T) {
-	type fields struct {
-		r repository
-	}
-	type args struct {
-		hallId int
-		date   string
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		want    []entity.CinemaSession
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			s := service{
-				r: tt.fields.r,
-			}
-			got, err := s.SessionsForHall(tt.args.hallId, tt.args.date)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("SessionsForHall() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("SessionsForHall() got = %v, want %v", got, tt.want)
-			}
-		})
-	}
+func (m *mockRepo) SessionsForHall(hallId int, date string) ([]entity.CinemaSession, error) {
+	return m.sessions, m.err
+}
+
+func TestAllSessions(t *testing.T) {
+	repo := mockRepo{}
+	t.Run("successful sessions get", func(t *testing.T) {
+		sessions := []entity.CinemaSession{{}, {}}
+		repo.sessions = sessions
+		repo.err = nil
+
+		s := New(&repo)
+		serviceSessions, err := s.AllSessions("2024-05-18", 0, 10)
+		assert.NoError(t, err)
+		assert.Len(t, serviceSessions, 2)
+	})
+
+	t.Run("no cinema sessions were found", func(t *testing.T) {
+		var sessions []entity.CinemaSession
+		repo.sessions = sessions
+		repo.err = ErrCinemaSessionsNotFound
+
+		s := New(&repo)
+		serviceSessions, err := s.AllSessions("2024-05-18", 0, 10)
+		assert.Equal(t, err, ErrCinemaSessionsNotFound)
+		assert.Len(t, serviceSessions, 0)
+	})
+
+	t.Run("repository error", func(t *testing.T) {
+		var sessions []entity.CinemaSession
+		repo.sessions = sessions
+		repo.err = errors.New("something went wrong")
+
+		s := New(&repo)
+		serviceSessions, err := s.AllSessions("2024-05-18", 0, 10)
+		assert.Equal(t, ErrInternalError, err)
+		assert.Len(t, serviceSessions, 0)
+	})
+}
+
+func TestSessionsForHall(t *testing.T) {
+	repo := mockRepo{}
+	t.Run("successful sessions get", func(t *testing.T) {
+		sessions := []entity.CinemaSession{{}, {}}
+		repo.sessions = sessions
+		repo.err = nil
+
+		s := New(&repo)
+		serviceSessions, err := s.AllSessions("2024-05-18", 0, 10)
+		assert.NoError(t, err)
+		assert.Len(t, serviceSessions, 2)
+	})
+
+	t.Run("no cinema sessions were found", func(t *testing.T) {
+		var sessions []entity.CinemaSession
+		repo.sessions = sessions
+		repo.err = ErrCinemaSessionsNotFound
+
+		s := New(&repo)
+		serviceSessions, err := s.AllSessions("2024-05-18", 0, 10)
+		assert.Equal(t, err, ErrCinemaSessionsNotFound)
+		assert.Len(t, serviceSessions, 0)
+	})
+
+	t.Run("repository error", func(t *testing.T) {
+		var sessions []entity.CinemaSession
+		repo.sessions = sessions
+		repo.err = errors.New("something went wrong")
+
+		s := New(&repo)
+		serviceSessions, err := s.AllSessions("2024-05-18", 0, 10)
+		assert.Equal(t, ErrInternalError, err)
+		assert.Len(t, serviceSessions, 0)
+	})
 }
