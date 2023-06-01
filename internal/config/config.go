fb35b6a7538f1663c8c9bdf5cd3c1da5b1364fbc
rubiezzy <rubiezzy@student.21-school.ru>
2023-06-01 17:23:22 +0400

    Add default value to tha db configuration

--- internal/config/config.go
+++ internal/config/config.go
@@ -12,7 +12,7 @@ var timeZone = time.FixedZone("UTC+4", 4*60*60)
 type Config struct {
 	Port      string `env:"PORT,default=8080"`
 	JWTSecret string `env:"JWT_SECRET,default=secret-key"`
-	Db        string `env:"DATABASE_URL"`
+	Db        string `env:"DATABASE_URL,default=postgresql://postgres:2587@localhost:5432/cinema?sslmode=disable"`
 	TokenExp  int    `env:"TOKEN_EXP_IN_HOURS,default=24"`
 	TimeZone  *time.Location
 }
