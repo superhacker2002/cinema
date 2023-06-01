aa673c98d0ab256905aa30f2d964c35f8ce9f2bf
rubiezzy <rubiezzy@student.21-school.ru>
2023-06-01 13:05:42 +0400

    Make repo field exported

--- internal/hall/service/service.go
+++ internal/hall/service/service.go
@@ -31,15 +31,15 @@ type repository interface {
 }
 
 type Service struct {
-	r repository
+	R repository
 }
 
 func (s Service) Halls() ([]Hall, error) {
-	return s.r.Halls()
+	return s.R.Halls()
 }
 
 func (s Service) HallById(id int) (Hall, error) {
-	hall, err := s.r.HallById(id)
+	hall, err := s.R.HallById(id)
 	if errors.Is(err, ErrHallNotFound) {
 		return Hall{}, ErrHallNotFound
 	}
@@ -50,27 +50,27 @@ func (s Service) HallById(id int) (Hall, error) {
 }
 
 func (s Service) CreateHall(name string, capacity int) (hallId int, err error) {
-	return s.r.CreateHall(name, capacity)
+	return s.R.CreateHall(name, capacity)
 }
 
 func (s Service) UpdateHall(id int, name string, capacity int) error {
-	ok, err := s.r.HallExists(id)
+	ok, err := s.R.HallExists(id)
 	if err != nil {
 		return ErrInternalError
 	}
 	if !ok {
 		return ErrHallNotFound
 	}
-	return s.r.UpdateHall(id, name, capacity)
+	return s.R.UpdateHall(id, name, capacity)
 }
 
 func (s Service) DeleteHall(id int) error {
-	ok, err := s.r.HallExists(id)
+	ok, err := s.R.HallExists(id)
 	if err != nil {
 		return ErrInternalError
 	}
 	if !ok {
 		return ErrHallNotFound
 	}
-	return s.r.DeleteHall(id)
+	return s.R.DeleteHall(id)
 }
