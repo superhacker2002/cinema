31d3bf942f7e01ac3eb78c20b93a1014a0a37b6c
rubiezzy <rubiezzy@student.21-school.ru>
2023-06-05 16:38:28 +0400

    Change HttpHandler service field visibility

--- internal/movie/handler/http.go
+++ internal/movie/handler/http.go
@@ -34,11 +34,11 @@ type Service interface {
 }
 
 type HTTPHandler struct {
-	S Service
+	s Service
 }
 
 func New(router *mux.Router, s Service) HTTPHandler {
-	handler := HTTPHandler{S: s}
+	handler := HTTPHandler{s: s}
 	handler.setRoutes(router)
 
 	return handler
@@ -55,7 +55,7 @@ func (h HTTPHandler) setRoutes(router *mux.Router) {
 }
 
 func (h HTTPHandler) getMoviesHandler(w http.ResponseWriter, _ *http.Request) {
-	cinemaHalls, err := h.S.Movies()
+	cinemaHalls, err := h.s.Movies()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -80,7 +80,7 @@ func (h HTTPHandler) createMovieHandler(w http.ResponseWriter, r *http.Request) 
 		return
 	}
 
-	id, err := h.S.CreateMovie(movie.Title, movie.Genre, movie.ReleaseDate, movie.Duration)
+	id, err := h.s.CreateMovie(movie.Title, movie.Genre, movie.ReleaseDate, movie.Duration)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -96,7 +96,7 @@ func (h HTTPHandler) getMovieHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	movie, err := h.S.MovieById(movieId)
+	movie, err := h.s.MovieById(movieId)
 	if errors.Is(err, service.ErrMoviesNotFound) {
 		http.Error(w, err.Error(), http.StatusNotFound)
 		return
@@ -132,7 +132,7 @@ func (h HTTPHandler) updateMovieHandler(w http.ResponseWriter, r *http.Request) 
 		return
 	}
 
-	err = h.S.UpdateMovie(movieId, movie.Title, movie.Genre, movie.ReleaseDate, movie.Duration)
+	err = h.s.UpdateMovie(movieId, movie.Title, movie.Genre, movie.ReleaseDate, movie.Duration)
 	if errors.Is(err, service.ErrMoviesNotFound) {
 		http.Error(w, err.Error(), http.StatusNotFound)
 		return
@@ -153,7 +153,7 @@ func (h HTTPHandler) deleteMovieHandler(w http.ResponseWriter, r *http.Request) 
 		return
 	}
 
-	err = h.S.DeleteMovie(movieId)
+	err = h.s.DeleteMovie(movieId)
 	if errors.Is(err, service.ErrMoviesNotFound) {
 		http.Error(w, err.Error(), http.StatusNotFound)
 		return
@@ -173,7 +173,7 @@ func (h HTTPHandler) watchedMoviesHandler(w http.ResponseWriter, r *http.Request
 		http.Error(w, ErrInvalidUserId.Error(), http.StatusBadRequest)
 		return
 	}
-	movies, err := h.S.WatchedMovies(userId)
+	movies, err := h.s.WatchedMovies(userId)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
