ec63387ee3ac97b2eca7e2312f137fa4adc7aeb1
rubiezzy <rubiezzy@student.21-school.ru>
2023-06-05 13:02:31 +0400

    Make repository not exported

--- internal/hall/service/service.go
+++ internal/hall/service/service.go
@@ -30,15 +30,15 @@ type repository interface {
 }
 
 type Service struct {
-	R repository
+	r repository
 }
 
 func New(r repository) Service {
-	return Service{R: r}
+	return Service{r: r}
 }
 
 func (s Service) Halls() ([]Hall, error) {
-	halls, err := s.R.Halls()
+	halls, err := s.r.Halls()
 	if err != nil {
 		return []Hall{}, ErrInternalError
 	}
@@ -46,7 +46,7 @@ func (s Service) Halls() ([]Hall, error) {
 }
 
 func (s Service) HallById(id int) (Hall, error) {
-	hall, err := s.R.HallById(id)
+	hall, err := s.r.HallById(id)
 	if errors.Is(err, ErrHallNotFound) {
 		return Hall{}, ErrHallNotFound
 	}
@@ -57,7 +57,7 @@ func (s Service) HallById(id int) (Hall, error) {
 }
 
 func (s Service) CreateHall(name string, capacity int) (hallId int, err error) {
-	id, err := s.R.CreateHall(name, capacity)
+	id, err := s.r.CreateHall(name, capacity)
 	if err != nil {
 		return 0, ErrInternalError
 	}
@@ -65,7 +65,7 @@ func (s Service) CreateHall(name string, capacity int) (hallId int, err error) {
 }
 
 func (s Service) UpdateHall(id int, name string, capacity int) error {
-	found, err := s.R.UpdateHall(id, name, capacity)
+	found, err := s.r.UpdateHall(id, name, capacity)
 	if err != nil {
 		return ErrInternalError
 	}
@@ -76,7 +76,7 @@ func (s Service) UpdateHall(id int, name string, capacity int) error {
 }
 
 func (s Service) DeleteHall(id int) error {
-	found, err := s.R.DeleteHall(id)
+	found, err := s.r.DeleteHall(id)
 	if err != nil {
 		return ErrInternalError
 	}
